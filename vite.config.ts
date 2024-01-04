87bd64c73988eba284c0845800b4f29fca6e1083
Fatih Altinok <fatihaltinok@live.com>
2024-01-05 01:26:56 +0300

    Bump remix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
+
+installGlobals();
 
 export default defineConfig({
   plugins: [
