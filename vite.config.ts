d70f819e8d678cb9e173b46b215625d3495e938e
Fatih Altinok <fatihaltinok@live.com>
2024-11-02 17:41:00 +0300

    Fix types

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [
+    reactRouter({
+      ssr: true,
+    }),
+  ],
   server: {
     port: 3000,
   },
